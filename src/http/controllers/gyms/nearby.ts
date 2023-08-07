af590d2e70b4915a00bf2febf15d413e95a9b6ea
Marcio Dauri <mdauri@gmail.com>
2023-08-06 23:07:19 -0300

    feat: gyms routes

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearByGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearByGymsQuerySchema.parse(request.body)
+
+  const fetchNearByGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const gyms = await fetchNearByGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
