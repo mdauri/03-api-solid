c60b4b1612e27715e45f0f68ae1b0a7229854e2f
Marcio Dauri <mdauri@gmail.com>
2023-08-11 21:22:36 -0300

    test: role based access

--- src/http/controllers/gyms/nearby.spec.ts
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -13,7 +13,7 @@ describe('NearBy Gyms (e2e)', () => {
   })
 
   it('should be able to list near by gyms', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     await request(app.server)
       .post('/gyms')
